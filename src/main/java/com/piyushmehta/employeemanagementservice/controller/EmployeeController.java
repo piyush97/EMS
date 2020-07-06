faa76794560b1426189eb4f8be36647be28f547e
Piyush Mehta <me@piyushmehta.com>
2020-07-07 00:36:31 +0530

    using interface to call method

--- src/main/java/com/piyushmehta/employeemanagementservice/controller/EmployeeController.java
+++ src/main/java/com/piyushmehta/employeemanagementservice/controller/EmployeeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
   @Autowired
-  EmployeeServiceImpl employeeService;
+  EmployeeService employeeService;
 
   @RequestMapping("/welcome")
   public String welcome() {
--- src/main/java/com/piyushmehta/employeemanagementservice/service/EmployeeService.java
+++ src/main/java/com/piyushmehta/employeemanagementservice/service/EmployeeService.java
@@ -1,4 +1,8 @@
 package com.piyushmehta.employeemanagementservice.service;
 
+import com.piyushmehta.employeemanagementservice.dto.Employee;
+import java.util.List;
+
 public interface EmployeeService {
+  List<Employee> getAllEmployee();
 }

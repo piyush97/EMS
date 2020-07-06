be4ad21119285553a920382873df566b8bbfc7df
Piyush Mehta <me@piyushmehta.com>
2020-07-06 23:26:58 +0530

    init

---
+++ src/main/java/com/piyushmehta/employeemanagementservice/EmployeeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.piyushmehta.employeemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piyushmehta/employeemanagementservice/EmployeeManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piyushmehta.employeemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

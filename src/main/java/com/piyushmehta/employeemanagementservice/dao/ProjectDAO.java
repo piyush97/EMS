e410b6f24e33a15e58c00a10bbb14b5a01bc07a9
Piyush Mehta <me@piyushmehta.com>
2020-07-07 21:49:33 +0530

    sanitize

--- src/main/java/com/piyushmehta/employeemanagementservice/dao/ProjectDAO.java
+++ src/main/java/com/piyushmehta/employeemanagementservice/dao/ProjectDAO.java
@@ -1,7 +1,6 @@
 package com.piyushmehta.employeemanagementservice.dao;
 
 import com.piyushmehta.employeemanagementservice.dto.Project;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
